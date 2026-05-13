e286d43434658b3ab74831cb032c89f5dd016724
mariosanz910 <mario.sanzvillarejo24@campusfp.es>
2026-05-13 12:12:20 +0200

    Google adsense integrado
    Herramienta de google para mostrar anuncios en la web y monetizar el contenido integrada

---
+++ src/main/java/com/tfg/wikilib/controller/BaseController.java
@@ -0,0 +1,18 @@
+package com.tfg.wikilib.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+
+@ControllerAdvice
+public class BaseController {
+
+    @Value("${google.ads.publisher-id}")
+    private String googleAdsPublisherId;
+
+    @ModelAttribute
+    public void addGlobalAttributes(Model model) {
+        model.addAttribute("googleAdsPublisherId", googleAdsPublisherId);
+    }
+}
